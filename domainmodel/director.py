68f5903121d36deaeacbadd20deb240e2f8f32ad
hazel00 <hazelwoodward00@gmail.com>
2020-09-04 16:48:26 +1200

    Revert "completed domain model"
    This reverts commit 413c0e13f88f86f344434736141ea9e4ea893ae5.

--- domainmodel/director.py
+++ domainmodel/director.py
@@ -15,10 +15,24 @@ class Director:
         return f"<Director {self.__director_full_name}>"
 
     def __eq__(self, other):
-        return self.__director_full_name == other.__director_full_name
+        # TODO
+        pass
 
     def __lt__(self, other):
-        return self.__director_full_name < other.__director_full_name
+        # TODO
+        pass
 
     def __hash__(self):
-        return hash(self.__director_full_name)
+        # TODO
+        pass
+
+
+class TestDirectorMethods:
+
+    def test_init(self):
+        director1 = Director("Taika Waititi")
+        assert repr(director1) == "<Director Taika Waititi>"
+        director2 = Director("")
+        assert director2.director_full_name is None
+        director3 = Director(42)
+        assert director3.director_full_name is None
